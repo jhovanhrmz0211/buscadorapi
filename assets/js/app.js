83e0ba770577cc36dff96226274d2f77605bf3e3
Jhovan <perjhoal@gmail.com>
2025-01-04 21:50:16 -0600

    Actualizando datos fetch

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,50 +1,39 @@
-const inputBuscador = document.querySelector('.buscador-input');
-const btnBuscar = document.querySelector('.btn-buscador');
+const inputBuscador = document.querySelector(".buscador-input");
+const btnBuscar = document.querySelector(".btn-buscador");
+const main = document.querySelector(".main");
 
-const buscarImg = async (query) => {
+const getImages = async (query) => {
   const endpoint = `https://api.unsplash.com/search/photos?query=${query}&per_page=10&client_id=jMBn4JdVYyIHAl4cKVZ7ICCtjtZmXHml5iEczZA7BkE`;
   try {
     const response = await fetch(endpoint);
-    if (!response.ok) {
-      throw new Error('Error al obtener los datos');
-    }
-
     const data = await response.json();
-    mostrarResultados(data.results);
+    renderImages(data.results);
+    console.log(data);
   } catch (error) {
-    console.error('Error:', error);
+    console.error(error);
   }
 };
 
-const mostrarResultados = (imagenes) => {
-  const main = document.querySelector('.main');
-  main.innerHTML = ''; // Limpiar contenido previo
+const renderImages = (images) => {
+  main.innerHTML = "";
+  images.forEach((image) => {
+    const containerImg = document.createElement("div");
+    containerImg.classList.add("container-img");
 
-  if (imagenes.length === 0) {
-    alert('No se encontraron resultados');
-    return;
-  }
-
-  imagenes.forEach((imagen) => {
-    const contenedor = document.createElement('div'); // Crear un nuevo contenedor para cada imagen
-    contenedor.classList.add('imagen-contenedor'); // Agregar clase para estilos
-
-    const img = document.createElement('img');
-    img.src = imagen.urls.small;
-    img.alt = imagen.alt_description || 'Imagen de Unsplash';
-
-    contenedor.appendChild(img);
-    main.appendChild(contenedor);
+    const img = document.createElement("img");
+    img.src = image.urls.small;
+    img.alt = image.alt_description || "Imagen de Unsplash";
+    containerImg.appendChild(img);
+    main.appendChild(containerImg);
   });
 };
 
-btnBuscar.addEventListener('click', () => {
-  const key = inputBuscador.value; // Obtén el valor del input al hacer clic
-  const palabra = key.replace(/\s/g, '+'); // Reemplaza espacios con "+"
-  
-  if (palabra) {
-    buscarImg(palabra);
+btnBuscar.addEventListener("click", () => {
+  const textIn = inputBuscador.value;
+  const busqueda = textIn.replace(/\s/g, "+");
+  if (busqueda !== "") {
+    getImages(busqueda);
   } else {
-    alert('Ingresa una palabra clave');
+    alert("Introduce una busqueda");
   }
 });
